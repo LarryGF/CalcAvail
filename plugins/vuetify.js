9b2bcb0472bf934cf7ecfaa3774d35348433aa58
larrygf <larrywtf609@gmail.com>
2019-03-16 08:30:47 +0000

    Initial Commit

---
+++ plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuetify, {
+  VApp, // required
+  VNavigationDrawer,
+  VFooter,
+  VToolbar
+} from 'vuetify/lib'
+import { Ripple } from 'vuetify/lib/directives'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VToolbar
+  },
+  directives: {
+    Ripple
+  }
+})
