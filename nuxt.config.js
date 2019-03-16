14a1cb0cd71e379b5ccd4eef58f8f4fa214f14e3
LarryGF <larrywtf609@gmail.com>
2019-03-16 03:24:25 +0000

    Minor modifications to the description of the project

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'viyda-dayron',
+    title: 'vydia-frontend',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
